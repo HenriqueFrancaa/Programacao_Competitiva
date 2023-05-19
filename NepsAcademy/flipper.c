f65a11aa6b5d8e3a03641d2ad07d07b6096dcde2
HenriqueFrancaa <henrique23.franca@gmail.com>
2023-05-19 10:28:54 -0300

    Questao Flipper em C :

---
+++ NepsAcademy/flipper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int p1,p2;
+    scanf("%d %d", &p1, &p2);
+
+    if(p1 == 0){
+        printf("C");
+    }
+
+    if(p1 == 1 && p2 == 0){
+        printf("B");
+    }
+
+    if(p1 == 1 && p2 == 1){
+        printf("A");
+    }
+
+    return 0;
+
+
+/*QUESTÂO NEPSACADEMY - Fliperama em linguagem C :*/
+}

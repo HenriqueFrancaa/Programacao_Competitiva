f7b6c1696b3e0c24e1420be71128c743280e329a
HenriqueFrancaa <henrique23.franca@gmail.com>
2023-05-19 11:55:21 -0300

    Questão Watermelon em C:

---
+++ CodeForces/Watermelon.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int peso;
+    scanf("%d",&peso);
+
+    if(peso%2 == 0 && peso != 2){
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
+    return 0;
+
+/*QUESTÂO CodeForces - A.WATERMELON em linguagem C:*/
+}
+

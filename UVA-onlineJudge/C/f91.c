698f50f7419c485ae2f14060036d29c950dac398
HenriqueFrancaa <henrique23.franca@gmail.com>
2023-05-29 20:49:58 -0300

    Question solved

---
+++ UVA-onlineJudge/C/f91.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+int main(){
+
+    while (true){
+
+        int n, novo_numero;
+        scanf("%d", &n);
+
+        if(n <= 100){
+            novo_numero = 91;
+        }
+
+        else if(n >= 101){
+            novo_numero = n - 10;
+        }
+
+
+        if (n == 0){
+            break;
+        }
+
+        printf("f91(%d) = %d\n", n, novo_numero); 
+    }
+
+    return 0;
+
+}
